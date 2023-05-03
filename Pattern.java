5ffac521d0cd806d61453d7e9c2851b7f31ebbb4
Sankalp-Kalode <sankalpkalode1@gmail.com>
2023-05-03 08:24:18 +0530

    first commit

---
+++ Pattern.java
@@ -0,0 +1,25 @@
+/*
+ *   A
+ *   AB
+ *   ABC
+ *   ABCD
+ *   ABCDE
+ */
+public class Pattern {
+
+	public static void main(String[] args) {
+
+       char k='A';
+		for(int i=1;i<=5;i++)
+		{  
+			k='A';
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(k++);  
+			}
+			System.out.println();
+		}
+
+	}
+
+}
